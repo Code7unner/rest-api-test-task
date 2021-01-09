b13db523016c94d7d6764867089906c495a30037
Code7unner <Cherbadzhimaxim@gmail.com>
2021-01-09 16:04:21 +1000

    Delete unused functionality

--- users/internal/auth/auth.go
+++ users/internal/auth/auth.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"errors"
 	"github.com/code7unner/rest-api-test-task/users/models"
 	"github.com/dgrijalva/jwt-go"
 	"time"
@@ -13,7 +12,6 @@ func CreateToken(user *models.Users, secret string, expires int) (string, error)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["id"] = user.ID
 	claims["username"] = user.Username
-	claims["password"] = user.Password
 	claims["exp"] = time.Now().Add(time.Minute * time.Duration(expires)).Unix()
 
 	t, err := token.SignedString([]byte(secret))
@@ -22,22 +20,4 @@ func CreateToken(user *models.Users, secret string, expires int) (string, error)
 	}
 
 	return t, nil
-}
-
-func GetPasswordFromToken(tokenStr, secret string) (string, error) {
-	claims := jwt.MapClaims{}
-
-	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(secret), nil
-	})
-	if err != nil || !token.Valid {
-		return "", err
-	}
-
-	password, ok := claims["password"].(string)
-	if !ok {
-		return "", errors.New("could not parse claims")
-	}
-
-	return password, nil
 }
