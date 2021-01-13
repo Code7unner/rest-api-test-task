bfa0789e6233c9f402bdb9fffc5474779224c436
Code7unner <Cherbadzhimaxim@gmail.com>
2021-01-13 22:06:00 +1000

    Fix docker-compose deploy

--- db-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER temp WITH password 'qwerty123';
-    CREATE DATABASE test_db;
-    GRANT ALL PRIVILEGES ON DATABASE test_db TO temp;
-EOSQL
